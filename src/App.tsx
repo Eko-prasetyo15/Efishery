d1b7263f2dd33a54d12b21f040ccf8a7623db74b
Eko-Ekrut <eko@ekrut.com>
2022-10-21 10:52:03 +0700

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,23 +6,8 @@ import {
 } from "react-router-dom";
 import routes from "./routes"
 import Layout from "./View/Component/Layout"
-import { useDispatch, useSelector } from 'react-redux';
-import { getList } from './Action';
 
 function App() {
-  // const dispatch = useDispatch()
-  // const listData = useSelector((state: any) => state.listPerikanan.listData)
-  // console.log(listData, 'list')
-  // useEffect(() => {
-  //   dispatch<any>(getList())
-  // }, [])
-  // const renderWithLayout = (Component: <Any/>, props: any) => {
-  //   return (
-  //     <Layout {...props}>
-  //       <Component {...props} />
-  //     </Layout>
-  //   );
-  // };
 
   return (
     // @ts-ignore
@@ -39,28 +24,11 @@ function App() {
                 <Layout>
                   <route.component />
                 </Layout>}
-            // render={(props: any) => renderWithLayout(<route.component />, props)}
             />
           );
         })}
       </Routes>
     </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
